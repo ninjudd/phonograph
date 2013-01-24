4eaa0e89fbaaec4cb5c5e6c116f3086ec197c020
Justin Balthrop <git@justinbalthrop.com>
2013-01-23 21:06:05 -0800

    slice byte-buffers since gloss doesn't obey position - reading ranges seems to work now

--- src/flatland/phonograph/whisper.clj
+++ src/flatland/phonograph/whisper.clj
@@ -26,39 +26,50 @@
         header      (decode header-format byte-buffer false)]
     (keyed [path byte-buffer header])))
 
-(defn archive-include? [interval {:keys [density count]}]
-  (<= interval (* density count)))
+(defn- select-archive [interval archives]
+  (first (filter (fn [{:keys [density count]}]
+                   (<= interval (* density count)))
+                 archives)))
+
+(defn- slice-byte-buffer [byte-buffer position limit]
+  (-> byte-buffer
+      (.duplicate)
+      (.position position)
+      (.limit (or limit (.capacity byte-buffer)))
+      (.slice)))
+
+(defn- archive-byte-buffer [{:keys [density count offset]} byte-buffer]
+  (let [limit (+ offset (* (sizeof point-format) count))]
+    (slice-byte-buffer byte-buffer offset limit)))
 
 (defn- truncate [time density]
   (- time (mod time density)))
 
-(defn- slot [{:keys [density count]} base time]
-  (mod (/ (- time base) density) count))
+(defn- archive-offset [{:keys [density count]} base time]
+  (* (sizeof point-format)
+     (rem (/ (- time base) density) count)))
 
-(defn- decode-points [offset slot num byte-buffer]
-  (.position byte-buffer (+ offset (* slot (sizeof point-format))))
-  (decode (compile-frame (repeat num point-format)) byte-buffer false))
+(defn- decode-points [position limit byte-buffer]
+  (decode (repeated point-format :prefix :none)
+          (slice-byte-buffer byte-buffer position limit)))
 
 (defn- get-values [{:keys [offset density count] :as archive} byte-buffer from until]
-  (.position byte-buffer offset)
-  (let [from (truncate from density)
+  (let [byte-buffer (archive-byte-buffer archive byte-buffer)
+        from (truncate from density)
         until (truncate until density)
         base (first (decode point-format byte-buffer false))
         num (/ (- until from) density)]
     (if (zero? base)
       (repeat num nil)
-      (let [from-slot (slot archive base from)
-            until-slot (slot archive base until)]
-        (map (fn [[time value]]
-               (prn [time value])
+      (let [from-offset (archive-offset archive base from)
+            until-offset (archive-offset archive base until)]
+        (map (fn [[time value]] ;; remove points not within the range
                (when (<= from time until)
                  value))
-             (if (< from-slot until-slot)
-               (decode-points offset from-slot num byte-buffer)
-               (let [from-count (- count from-slot)
-                     until-count (- num from-count)]
-                 (concat (decode-points offset from-slot from-count byte-buffer)
-                         (decode-points offset 0 until-count byte-buffer)))))))))
+             (if (< from-offset until-offset)
+               (decode-points from-offset until-offset byte-buffer)
+               (concat (decode-points from-offset nil byte-buffer)
+                       (decode-points 0 until-offset byte-buffer))))))))
 
 (defn get-range [{:keys [header byte-buffer now]} from until]
   (when (< until from)
@@ -66,14 +77,12 @@
             (format "Invalid time interval: from time '%s' is after until time '%s'" from until))))
   (let [now (or now (quot (System/currentTimeMillis) 1000))
         oldest (- now (:max-retention header))]
-    (prn now oldest)
     (cond (< now from) {}
           (< until oldest) {}
           :else
           (let [from (max from oldest)
                 until (min now until)
-                archive (first (filter (partial archive-include? (- now from))
-                                       (:archives header)))
+                archive (select-archive (- now from) (:archives header))
                 density (:density archive)
                 values (get-values archive byte-buffer from until)]
             (keyed [from until density values])))))
