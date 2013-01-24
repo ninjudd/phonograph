dac8117010285bf08edd7f30da5b1abc7ba96bfe
Justin Balthrop <git@justinbalthrop.com>
2013-01-24 15:33:07 -0800

    slice byte buffers for each archive ahead of time

--- src/flatland/phonograph.clj
+++ src/flatland/phonograph.clj
@@ -11,22 +11,31 @@
    :aggregate (enum :int32 :average :sum :last :max :min)
    :max-retention :int32
    :propagation-threshold :float32
-   :archives (repeated (ordered-map
-                        :offset :int32
-                        :density :int32
-                        :count :int32))))
+   :archives (repeated (ordered-map :offset :int32
+                                    :density :int32
+                                    :count :int32))))
 
 (def point-format
-  (compile-frame
-   [:uint32 :float64]))
+  (compile-frame [:uint32 :float64]))
+
+(defn- slice-buffer [^ByteBuffer buffer position limit]
+  (-> (.duplicate buffer)
+      (.position position)
+      (.limit (or limit (.capacity buffer)))
+      (.slice)))
+
+(defn- slice-buffers [archives buffer]
+  (for [{:keys [offset count] :as archive} archives]
+    (let [limit (+ offset (* (sizeof point-format) count))]
+      (assoc archive
+        :buffer (slice-buffer buffer offset limit)))))
 
 (defn open-file [path]
-  (let [channel     (.getChannel (RandomAccessFile. path "rw"))
-        byte-buffer (.map channel FileChannel$MapMode/READ_WRITE 0 (.size channel))
-        header      (decode header-format byte-buffer false)]
-; pre-slice archive shizzle
-
-    (keyed [path byte-buffer header])))
+  (let [channel (.getChannel (RandomAccessFile. path "rw"))
+        buffer (.map channel FileChannel$MapMode/READ_WRITE 0 (.size channel))
+        header (-> (decode header-format buffer false)
+                   (update :archives slice-buffers buffer))]
+    (keyed [path buffer header])))
 
 (def aggregate-fn
   {:average #(double (/ (apply + %) (count %)))
@@ -40,17 +49,6 @@
                    (<= interval (* density count)))
                  archives)))
 
-(defn- slice-byte-buffer [byte-buffer position limit]
-  (-> byte-buffer
-      (.duplicate)
-      (.position position)
-      (.limit (or limit (.capacity byte-buffer)))
-      (.slice)))
-
-(defn- archive-buffer [{:keys [density count offset]} byte-buffer]
-  (let [limit (+ offset (* (sizeof point-format) count))]
-    (slice-byte-buffer byte-buffer offset limit)))
-
 (defn- trunc [density time]
   (- time (mod time density)))
 
@@ -61,28 +59,28 @@
 (defn- retention [{:keys [density count]}]
   (* density count))
 
-(defn- read-points [byte-buffer position limit]
+(defn- read-points [buffer position limit]
   (decode (repeated point-format :prefix :none)
-          (slice-byte-buffer byte-buffer position limit)))
+          (slice-buffer buffer position limit)))
 
-(defn- write-point! [byte-buffer position point]
+(defn- write-point! [buffer position point]
   (encode-to-buffer point-format
-                    (slice-byte-buffer byte-buffer position nil)
+                    (slice-buffer buffer position nil)
                     [point]))
 
 (defn- current-time []
   (quot (System/currentTimeMillis) 1000))
 
-(defn- base-time [byte-buffer]
-  (first (decode point-format byte-buffer false)))
+(defn- base-time [buffer]
+  (first (decode point-format buffer false)))
 
 (defn- get-values
-  "Get a range of values from an archive between from and until. The byte-buffer passed in must
+  "Get a range of values from an archive between from and until. The buffer passed in must
   be already sliced to only contain the bytes for this specific archive."
-  [{:keys [density] :as archive} byte-buffer from until]
+  [{:keys [density buffer] :as archive} from until]
   (let [from (trunc density from)
         until (trunc density until)
-        base (base-time byte-buffer)
+        base (base-time buffer)
         num (/ (- until from) density)]
     (if (zero? base)
       (repeat num nil)
@@ -92,52 +90,48 @@
                (when (<= from time until)
                  value))
              (if (< from-offset until-offset)
-               (read-points byte-buffer from-offset until-offset)
-               (concat (read-points byte-buffer from-offset nil)
-                       (read-points byte-buffer 0 until-offset))))))))
+               (read-points buffer from-offset until-offset)
+               (concat (read-points buffer from-offset nil)
+                       (read-points buffer 0 until-offset))))))))
 
-(defn get-range [{:keys [header byte-buffer now]} from until]
+(defn get-range [{:keys [header buffer now]} from until]
   (when (< until from)
     (throw (IllegalArgumentException.
             (format "Invalid time interval: from time '%s' is after until time '%s'" from until))))
   (let [now (or now (current-time))
         oldest (- now (:max-retention header))]
-    (cond (< now from) {}
-          (< until oldest) {}
-          :else
-          (let [from (max from oldest)
-                until (min now until)
-                archive (select-archive (- now from) (:archives header))
-                buffer (archive-buffer archive byte-buffer)
-                values (get-values archive buffer from until)
-                density (:density archive)]
-            (keyed [from until density values])))))
+    (if (or (< now from)
+            (< until oldest))
+      {}
+      (let [from (max from oldest)
+            until (min now until)
+            archive (select-archive (- now from) (:archives header))
+            values (get-values archive from until)
+            density (:density archive)]
+        (keyed [from until density values])))))
 
 (defn- write-points!
   "Write points into the given archive. Points need to be already aggregated to match the resolution
-  of the archive, as existing points in the archive are overwritten. The byte-buffer passed in must
+  of the archive, as existing points in the archive are overwritten. The buffer passed in must
   be already sliced to only contain the bytes for this specific archive."
-  [{:keys [density] :as archive} byte-buffer points]
-  (let [base (or (base-time byte-buffer)
+  [{:keys [density buffer] :as archive} points]
+  (let [base (or (base-time buffer)
                  (trunc density (ffirst points)))]
     (for [[time value] points]
       (let [time (trunc density time)
             offset (archive-offset archive base time)]
-        (write-point! byte-buffer offset [time value])))))
+        (write-point! buffer offset [time value])))))
 
-(defn add-points! [{:keys [header byte-buffer now]} & points]
+(defn add-points! [{:keys [header buffer now]} & points]
   (let [archive (first (:archives header))
         now (trunc (:density archive) (or now (current-time)))
-        buffer (archive-buffer archive buffer)
         points (filter #(< (- now (retention archive)) % now) points)]
-    (write-points! archive byte-buffer points)
+    (write-points! archive points)
     ;; propagate to lower resolution archives
     (doseq [[higher lower] (partition 2 1 archives)]
-      (let [scale (/ (:density lower) (:density higher))
-            oldest (- now (retention higher))
-            values (get-values higher oldest now)
-            buffer (archive-buffer archive buffer)]
-        (->> (partition scale values)
-             (map (aggregate-fn (:aggregate header)))
-             (map vector (range oldest now (:density higher)))
-             (write-points! archive buffer))))))
+      (let [oldest (- now (retention higher))]
+        (write-points!
+         (->> (get-values higher oldest now)
+              (partition (/ (:density lower) (:density higher))) ; will divide evenly
+              (map (aggregate-fn (:aggregate header)))
+              (map vector (range oldest now (:density higher)))))))))
