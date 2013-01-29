1a6bf5d786bf3b7cb32eda7c9bcb4e98b8a06c73
Justin Balthrop <git@justinbalthrop.com>
2013-01-29 10:08:29 -0800

    version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.flatland/phonograph "0.1.0-SNAPSHOT"
+(defproject org.flatland/phonograph "0.1.0"
   :description "A round-robin database based on graphite's whisper."
   :url "http://github.com/flatland/phonograph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.flatland/useful "0.9.0"]
-                 [gloss "0.2.2-SNAPSHOT"]])
+                 [gloss "0.2.2-beta5"]])
